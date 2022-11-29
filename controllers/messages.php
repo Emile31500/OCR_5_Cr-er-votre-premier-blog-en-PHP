a441eed57f7cafb759ad84d46b16207c9b60468a
Emile <emile00013@gmail.com>
2022-11-29 11:07:09 +0100

    envoyer()_right_not_test

--- controllers/messages.php
+++ controllers/messages.php
@@ -1,11 +1,38 @@
 <?php
 
-class Accueil extends Controller {
+class Messages extends Controller {
 
 
     public function envoyer(){
 
-        
+        if (isset($_POST["name"]) && !empty($_POST["name"]) &&
+            isset($_POST["firstname"]) && !empty($_POST["firstname"]) &&
+            isset($_POST["message"]) && !empty($_POST["message"]) &&
+            isset($_POST["email"]) && !empty($_POST["email"])) {
+
+                if(isset($_POST["objet"]) && !empty($_POST["objet"])){
+
+                    $objet = $_POST['objet'];
+
+                } 
+
+                $array = [
+                    "nom" => $_POST["name"],
+                    "prenom" => $_POST["firstname"],
+                    "message" => $_POST["message"],
+                    "email" => $_POST["email"],
+                    "objet" => $objet,
+                    "est_supprimer" => 0
+                    
+                ];
+
+                $this->loadModel("Message");
+                $this->Message->table="Messages";
+                $messages = $this->Message->insert($array);
+
+
+            }
+            
         $this->render_page("index", [
                                     "title" => "Accueil",
                                     "description" => [
