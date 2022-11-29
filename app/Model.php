307dea18fa36a16b75d860f313b2a71c89e0c140
Emile <emile00013@gmail.com>
2022-11-29 11:10:44 +0100

    add_Model.php

--- app/Model.php
+++ app/Model.php
@@ -5,7 +5,12 @@ abstract class Model {
     private $data_base = "mon_blog";
     private $user = "root";
     private $password = "";
-   
+    
+    public $array_selector_request = [];
+    public $array_value_request = [];
+
+    public $public = "";
+    public $id = "";
     
     public $connexion;
 
@@ -18,11 +23,101 @@ abstract class Model {
 
             echo 'Erreur : '.$exception->getMessage();
 
+            
         }
 
 
     }
 
+    public function getAll(){
+
+        getConnection();
+        $request = "SELECT * FROM ".$this->table." WHERE 1";
+        $query = $this->connexion->prepare($request);
+        $query->execute();
+        return $query->fetchAll();
+    
+    }
+
+    public function getOne(){
+
+        getConnection();
+        $request = "SELECT * FROM ".$this->table." WHERE id=".$this->id;
+        $query = $this->connexion->prepare($request);
+        $query->execute();
+        return $query->fetch();
+    
+    }
+
+    public function getBy(){
+
+        getConnection();
+        foreach ($array_selector_request as $selector){
+
+            $where = $where.array_keys($selector)."=".array_values($selector)." AND ";
+
+        }
+
+        $where = substr($where, 0, -5);
+
+        $request = "SELECT * FROM ".$this->table." WHERE ".$where;
+        $query = $this->connexion->prepare($request);
+        $query->execute();
+        return $query->fetchAll();
+
+    }
+
+    public function insert(){
+
+        getConnection();
+        $array_values = array_values($this->array_selector_request);
+        $array_keys = array_keys($this->array_selector_request);
+
+        foreach ($array_keys as $array_key){
+
+            $keys = $keys."`".$array_key."`, ";
+
+        }
+
+        foreach ($array_values as $array_value){
+
+            $values = $values."`".$array_value."`, ";
+
+        }
+
+        $keys = substr($keys, 0, -2);
+        $values = substr($values, 0, -2);
+
+        $request = "INSERT INTO ".$this->table."(".$keys.") VALUES (".$values.")";
+        $query = $this->connexion->prepare($request);
+        return $query->execute();
+
+    }
+
+    public function update(){
+
+        getConnection();
+
+        foreach ($array_value_request as $value){
+
+            $values = "`".$where.array_keys($value)."`=".array_values($value).", ";
+
+        }
+
+
+        foreach ($array_selector_request as $selector){
+
+            $selector = "`".$selector.array_keys($selector)."`=".array_values($selector)." AND ";
+
+        }
+       
+        $values = substr($values, 0, -5);
+        $selector = substr($selector, 0, -5);
+
+        $request = "UPDATE ".$this->table." SET ".$value." WHERE ".$selector;
+        $query = $this->connexion->prepare($request);
+        return $query->execute();
+    }
 
 }
 
