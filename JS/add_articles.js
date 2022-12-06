0b4c7477dfad081db701a5a4db97462ef3ddd556
Emile <emile00013@gmail.com>
2022-12-06 11:45:41 +0100

    preview_article_main_image

--- JS/add_articles.js
+++ JS/add_articles.js
@@ -1,33 +1,60 @@
 DecoupledEditor
-		.create( document.querySelector( '#editor' ), {
-			// toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
-		} )
-		.then( editor => {
-			const toolbarContainer = document.querySelector( 'main .toolbar-container' );
+        .create( document.querySelector( '#editor' ), {
+            // toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+        } )
+        .then( editor => {
+            const toolbarContainer = document.querySelector( 'main .toolbar-container' );
 
-			toolbarContainer.prepend( editor.ui.view.toolbar.element );
+            toolbarContainer.prepend( editor.ui.view.toolbar.element );
 
-			window.editor = editor;
-		} )
-		.catch( err => {
-			console.error( err.stack );
-		} );
-    
+            window.editor = editor;
+        } )
+        .catch( err => {
+            console.error( err.stack );
+        } );
+
 var alert_zone = document.getElementById("alert_zone");
 var title_image = document.getElementById("title_image");
+var pre_visu_img = document.getElementById("pre_visu_img");
 var title_article = document.getElementById("title_article");
 var ajouter_article = document.getElementById("ajouter_article");
 var text_area_article =  document.getElementById("text_area_article");
 var formulaire_ajout_article = document.getElementById("formulaire_ajout_article");
 var data;
-var xml = new XMLHttpRequest()
+var xml = new XMLHttpRequest();
+
+title_image.addEventListener("input", function (event){
+
+
+    if(event.target.files.length > 0){
+
+        var src = URL.createObjectURL(event.target.files[0]);
+        pre_visu_img.src = src;
+        pre_visu_img.style.display = "block";
+    }
+
+});
+  
+
 
 
 formulaire_ajout_article.addEventListener("submit", function(event){
 
     event.preventDefault();
-    var editor = document.getElementById("editor");
+    let editor = document.getElementById("editor");
     text_area_article.value = editor.innerHTML;
+
+    if (editor.innerHTML.length > 16384){
+
+                    
+        alert_zone.className = "";
+        alert_zone.classList.add("alert");
+        alert_zone.classList.add("alert-warning");
+        alert_zone.innerHTML = "Attetion : l'article est trop long et ne peut donc pas être enregistré";
+        return false;
+
+
+    }
 
     data = new FormData(this)
     xml.responseType = "document";
