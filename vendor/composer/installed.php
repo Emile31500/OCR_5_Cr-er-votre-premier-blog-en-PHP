71999a6d32ca54e2652e3d1ea1fcf10167029c09
Emile <emile00013@gmail.com>
2022-12-12 10:06:42 +0100

    publish_article

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'emile/projet5',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f2b1f3edc3ffb6cc0e7ac8f33998a40ce019ecd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'emile/projet5' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f2b1f3edc3ffb6cc0e7ac8f33998a40ce019ecd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'pretty_version' => 'v1.27.0',
+            'version' => '1.27.0.0',
+            'reference' => '5bbc823adecdae860bb64756d639ecfec17b050a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twig/twig' => array(
+            'pretty_version' => 'v1.44.7',
+            'version' => '1.44.7.0',
+            'reference' => '0887422319889e442458e48e2f3d9add1a172ad5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
